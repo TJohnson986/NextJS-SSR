978ec41ed99bab377b6a2a8d44c3fcc0849f8e37
Tyler Johnson <tyler@csdconsultants.us>
2021-11-25 07:57:17 -0700

    creates first-page.js, modifies index.js link to direct to first-page.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -12,8 +13,11 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        <h1 className="title">
+          Read{' '}
+          <Link href="/first-page">
+            <a>this page!</a>
+          </Link>
         </h1>
 
         <p className={styles.description}>
