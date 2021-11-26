0ccd3e59f88c29001970ca512ece972d3af00bc3
Tyler Johnson <tyler@csdconsultants.us>
2021-11-26 07:33:25 -0700

    adds Head component and title

--- pages/first-page.js
+++ pages/first-page.js
@@ -1,8 +1,12 @@
 import Link from 'next/link'
+import Head from 'next/head'
 
 export default function FirstPage() {
   return (
     <>
+    <Head>
+        <title>First Page</title>
+    </Head>
       <h1>First Page</h1>
       <h2>
         <Link href="/">
