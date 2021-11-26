097b25d4c85e8388313c48a7db283315c005e421
Tyler Johnson <tyler@csdconsultants.us>
2021-11-26 08:05:01 -0700

    adds CSS modules and styling, modifies content on index.js and first-page.js

--- pages/first-page.js
+++ pages/first-page.js
@@ -1,18 +1,21 @@
 import Link from 'next/link'
 import Head from 'next/head'
+import Layout from '../components/layout.js';
 
 export default function FirstPage() {
-  return (
-    <>
-    <Head>
-        <title>First Page</title>
-    </Head>
-      <h1>First Page</h1>
-      <h2>
-        <Link href="/">
-          <a>Back to home</a>
-        </Link>
-      </h2>
-    </>
-  )
+    return (
+        <>
+            <Layout>
+                <Head>
+                    <title>First Page</title>
+                </Head>
+                <h1>First Page</h1>
+                <h2>
+                    <Link href="/">
+                        <a>Back to home</a>
+                    </Link>
+                </h2>
+            </Layout>
+        </>
+    )
 }
